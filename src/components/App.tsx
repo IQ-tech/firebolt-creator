850db4c9f74f55e741ae3febd3228f0431d7013a
Sabrina <helsabrinacarvalho@hotmail.com>
2022-03-25 15:31:43 -0300

    refactor: :recycle: code moved to InternalLayouts component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,37 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import BaseStyles from "./BaseStyles";
 import theme from "@/theme";
 import EditorPage from "@/pages/EditorPage";
+import RegisterPage from "@/pages/RegisterPage";
 import InternalLayouts from "./layout/InternalLayouts";
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div
-        css={(theme) => ({
-          backgroundColor: theme?.colors?.["cream-tusk"],
-          width: "100vw",
-          height: "100vh",
-          paddingTop: "48px"
-        })}
-      >
         <BaseStyles />
         <Routes>
-          <Route
-            path="/"
-            element={<p style={{ fontSize: "40px" }}>HOME<a href="/app"> Click GO /app </a></p>}
-          />
-
+          <Route path="/" element={<RegisterPage />}/>
           <Route path="/app" element={<InternalLayouts />}>
             <Route path="editor" element={<EditorPage />} />
           </Route>
-
           <Route
             path="*"
             element={<p style={{ fontSize: "40px" }}>NOT FOUND</p>}
           />
         </Routes>
-      </div>
     </ThemeProvider>
   );
 }
