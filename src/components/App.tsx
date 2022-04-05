3fcddaa4707401d40eefde4e162abe8e2cf7bbf6
Sabrina <helsabrinacarvalho@hotmail.com>
2022-04-05 09:51:43 -0300

    feat: :truck: new routes added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,21 @@
 import { ThemeProvider } from "@emotion/react";
 import { Routes, Route } from "react-router-dom";
-import BaseStyles from "./BaseStyles";
-import theme from "@/theme";
-import EditorPage from "@/pages/EditorPage";
 import RegisterPage from "@/pages/RegisterPage";
+import MainPage from "@/pages/MainPage";
 import InternalLayouts from "./layout/InternalLayouts";
+import theme from "@/theme";
+import BaseStyles from "./BaseStyles";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
         <BaseStyles />
         <Routes>
-          <Route path="/" element={<RegisterPage />}/>
-          <Route path="/app" element={<InternalLayouts />}>
-            <Route path="editor" element={<EditorPage />} />
-            <Route path="tracks" element={<p>Tracks</p>} />
-            <Route path="jschema" element={<p>Json Schema</p>} />
+          <Route path="/" element={<RegisterPage/>}/>
+          <Route path="/app" element={<InternalLayouts/>}>
+            <Route path="main" element={<MainPage/>} />
+            <Route path="tracks" element={<p>Tracks</p>}/>
+            <Route path="jschema" element={<p>Json Schema</p>}/>
           </Route>
           <Route
             path="*"
