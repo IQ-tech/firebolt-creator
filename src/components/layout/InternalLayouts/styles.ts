9f91753b38344d3afc7a6ac22d789d57c3ca9b53
Sabrina <helsabrinacarvalho@hotmail.com>
2022-04-05 09:50:33 -0300

    style: :lipstick: new colors and styles added

--- src/components/layout/InternalLayouts/styles.ts
+++ src/components/layout/InternalLayouts/styles.ts
@@ -1,11 +1,11 @@
-export const layoutStyles = { padding: '28px 0'}
+export const layoutStyles = { padding: '28px 0 0'}
 
-export const contentStyles = { padding: '50px 28px'}
+export const contentStyles = { padding: '50px 28px 22px'}
 
 export const pageHeaderStyles = {padding: "16px 24px 0"}
 
-export const breadcrumbContent = {padding: '1px 24px 0', backgroundColor: "#fff",}
+export const breadcrumbContent = (theme) => ({padding: '1px 24px 0', backgroundColor: theme?.colors?.white,})
 
 export const breadcrumbStyles = { marginTop: '16px' }
 
-export const moreButton = {borderColor: "#D9D9D9"}
+export const moreButton = (theme) => ({borderColor: theme?.colors?.["grey85"]})
