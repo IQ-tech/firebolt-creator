e9c1102e9e824b43ac6b0c74e0ce8c5e59aa28f5
Italo Fontes <italo5123@hotmail.com>
2022-04-07 16:10:45 -0300

    docs: :bulb: trigger build

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
+
+// trigger build
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
