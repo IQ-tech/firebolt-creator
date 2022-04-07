fa399998a41a957349c45417ede814db07cae4dd
Italo Fontes <italo5123@hotmail.com>
2022-04-07 16:58:33 -0300

    fix: :truck: changed to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+/* import { BrowserRouter } from "react-router-dom"; */
+import { HashRouter } from "react-router-dom";
 import App from "./components/App";
-
-
-// trigger build
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* 
     <BrowserRouter basename="/firebolt-creator">
       <App />
-    </BrowserRouter>
+    </BrowserRouter> 
+    */}
+    {/* using hash router due to github pages problems */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
